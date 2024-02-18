d64bb4388b7eb2a5ec8eb7842c7c03c9b3c78056
Nikki <ngolev.bus@gmail.com>
2024-02-18 18:26:45 +1100

    initial commit

---
+++ src/components/Articles.tsx
@@ -0,0 +1,9 @@
+const Articles = () => {
+    return (
+        <ul className="list-group">
+            <li className="list-group-item">An item</li>
+        </ul>
+    );
+};
+
+export default Articles;
