165794865ed4a0f1146dffdfc58278e4abe85a75
Nikita Golev <ngolev.bus@gmail.com>
2024-12-11 17:12:20 +1100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cyberpunk-news/', // Set the base URL to match your GitHub Pages repository
   plugins: [react()],
 })
